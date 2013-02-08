76538e82fb270ba7783b27999689d6e6bad0d619
qeremy <qeremy@gmail.com>
2013-02-08 18:32:51 +0200

    ...

--- unicode-tools.php
+++ unicode-tools.php
@@ -186,25 +186,27 @@ function str_pad_unicode($str, $pad_len, $pad_str = ' ', $dir = STR_PAD_RIGHT) {
     if (!$pad_len || !$pad_str_len || $pad_len <= $str_len) {
         return $str;
     }
-    
+
     $result = null;
     if ($dir == STR_PAD_BOTH) {
         $length = ($pad_len - $str_len) / 2;
         $repeat = ceil($length / $pad_str_len);
-        $result = mb_substr(str_repeat($pad_str, $repeat), 0, floor($length)) 
-                    . $str 
-                       . mb_substr(str_repeat($pad_str, $repeat), 0, ceil($length));
+        $result = mb_substr(str_repeat($pad_str, $repeat), 0, floor($length))
+                . $str
+                . mb_substr(str_repeat($pad_str, $repeat), 0, ceil($length));
     } else {
         $repeat = ceil($str_len - $pad_str_len + $pad_len);
         if ($dir == STR_PAD_RIGHT) {
             $result = $str . str_repeat($pad_str, $repeat);
             $result = mb_substr($result, 0, $pad_len);
         } else if ($dir == STR_PAD_LEFT) {
-            $result = str_repeat($pad_str, $repeat) . $str;
-            $result = mb_substr($result, -$pad_len);
+            $result = str_repeat($pad_str, $repeat);
+            $result = mb_substr($result, 0, 
+                        $pad_len - (($str_len - $pad_str_len) + $pad_str_len))
+                    . $str;
         }
     }
-    
+
     return $result;
 }
 
